851becf57f812d13abb58fc539b13195c00ff8c8
Hongi Keam <hongikeam@daumcorp.com>
2014-04-16 15:37:42 +0900

    add dragleave event handler

--- draganddrop.js
+++ draganddrop.js
@@ -52,6 +52,7 @@ angular.module("ngDragDrop",[])
         '$rootScope',
         function ($parse, $rootScope) {
             return function (scope, element, attr) {
+                var dragging = 0; //Ref. http://stackoverflow.com/a/10906204
                 var dropChannel = "defaultchannel";
                 var dragChannel = "";
                 var dragEnterClass = attr.dragEnterClass || "on-drag-enter";
@@ -70,7 +71,15 @@ angular.module("ngDragDrop",[])
                     return false;
                 }
 
+                function onDragLeave(e) {
+                  dragging--;
+                  if (dragging == 0) {
+                    element.removeClass(dragHoverClass);
+                  }
+                }
+
                 function onDragEnter(e) {
+                    dragging++;
                     $rootScope.$broadcast("ANGULAR_HOVER", dropChannel);
                     element.addClass(dragHoverClass);
                 }
@@ -98,6 +107,7 @@ angular.module("ngDragDrop",[])
 
                         element.bind("dragover", onDragOver);
                         element.bind("dragenter", onDragEnter);
+                        element.bind("dragleave", onDragLeave);
 
                         element.bind("drop", onDrop);
                         element.addClass(dragEnterClass);
@@ -113,6 +123,7 @@ angular.module("ngDragDrop",[])
 
                         element.unbind("dragover", onDragOver);
                         element.unbind("dragenter", onDragEnter);
+                        element.unbind("dragleave", onDragLeave);
 
                         element.unbind("drop", onDrop);
                         element.removeClass(dragHoverClass);
