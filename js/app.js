6910ce772a7e25337b4059d27b074ff71d34bc1c
Toilal <toilal.dev@gmail.com>
2015-03-14 11:04:24 +0100

    Fix bower.json version and bump angular to 1.3

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Ganaraj.Pr
- * Date: 15/10/13
- * Time: 14:48
- * To change this template use File | Settings | File Templates.
- */
-
 angular.module('app', [
     'hljs',
     'ang-drag-drop'
